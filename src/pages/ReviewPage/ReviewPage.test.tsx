10d45d71f679284be1900b4aa8da0c5b5ff75439
Christian Toney <christian@makuwro.com>
2024-10-23 10:48:06 -0400

    Update test

--- src/pages/ReviewPage/ReviewPage.test.tsx
+++ src/pages/ReviewPage/ReviewPage.test.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 it("submit reviews", () => {
 
+  // Type user name
+  const userNameBox = screen.getByLabelText("Username");
+  fireEvent.change(userNameBox, {target: {value: "Bob Davies"}});
+
   // Type content in review body
+  const contentBox = screen.getByLabelText("Description");
+  fireEvent.change(contentBox, {target: {value: "This totally is the best website ever :)"}});
 
   // Press submit button
+  const submitButton = screen.getByRole("button", {name: "Submit"});
+  fireEvent.click(submitButton);
 
   // Check if review is there with the correct string
+  expect();
 
 });
 
