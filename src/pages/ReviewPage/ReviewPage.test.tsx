73c8650d826f342cf21b34f9fc2430a2c5ca8ef8
Christian Toney <christian@makuwro.com>
2024-10-23 12:31:34 -0400

    Add initial code

--- src/pages/ReviewPage/ReviewPage.test.tsx
+++ src/pages/ReviewPage/ReviewPage.test.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import { render, screen, fireEvent } from "@testing-library/react";
+/**
+ * @jest-environment jsdom
+ */
 
-it("submit reviews", () => {
+import { render, screen, fireEvent, RenderResult } from "@testing-library/react";
+import ReviewPage from './ReviewPage';
+
+function getReviewPage(): RenderResult {
+
+  return render(<ReviewPage />);
+
+}
+
+it("Successfully submits reviews", () => {
 
   // Type user name
-  const userNameBox = screen.getByLabelText("Username");
-  fireEvent.change(userNameBox, {target: {value: "Bob Davies"}});
+  getReviewPage();
+  const usernameBox = screen.getByLabelText("Username") as HTMLInputElement;
+  fireEvent.change(usernameBox, {target: {value: "Bob Davies"}});
 
   // Type content in review body
-  const contentBox = screen.getByLabelText("Description");
+  const contentBox = screen.getByLabelText("Description") as HTMLInputElement;
   fireEvent.change(contentBox, {target: {value: "This totally is the best website ever :)"}});
 
   // Press submit button
-  const submitButton = screen.getByRole("button", {name: "Submit"});
+  const submitButton = screen.getByRole("button", {name: "Submit"}) as HTMLButtonElement;
   fireEvent.click(submitButton);
 
-  // Check if review is there with the correct string
-  expect();
+  // Verify the review box is empty.
+  expect(usernameBox.value).toBe("");
+  expect(contentBox.value).toBe("");
+
+  // Verify the review appears on the page.
 
 });
 
---
+++ src/pages/ReviewPage/ReviewPage.tsx
@@ -0,0 +1,42 @@
+import { useState, FormEvent } from "react";
+
+export default function ReviewPage() {
+
+  type Review = {
+    username: string;
+    description: string;
+  }
+
+  const [username, setUsername] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [reviewList, setReviewList] = useState<Review[]>([]);
+
+  function submitReview(event: FormEvent) {
+
+    // Prevent the page from refreshing.
+    event.preventDefault();
+
+    // Add the review to the list.
+    setReviewList([...reviewList, {username, description}]);
+    setUsername("");
+    setDescription("");
+
+  }
+  
+  return (
+    <main>
+      <form onSubmit={(event) => submitReview(event)}>
+        <section>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </section>
+        <section>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" />
+        </section>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+
+}
