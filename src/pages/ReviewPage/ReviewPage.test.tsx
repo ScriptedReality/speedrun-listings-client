10b1241f05858b44319eb600e95c07bd88b4b9a5
Christian Toney <christian@makuwro.com>
2024-10-23 10:34:11 -0400

    Add initial tests

---
+++ src/pages/ReviewPage/ReviewPage.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+
+it("submit reviews", () => {
+
+  // Type content in review body
+
+  // Press submit button
+
+  // Check if review is there with the correct string
+
+});
+
+it("shows reviewers' correct amount of stars", () => {
+
+  // Press stars
+
+  // Press submit button
+
+  // Check if review is there with the correct stars
+
+});
+
+it("deletes reviews", () => {
+
+  // Type random content in review body
+
+  // Press submit button
+
+  // Delete review
+
+});
