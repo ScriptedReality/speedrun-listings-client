9708d38bc221dfecbe2df45d3e5436e0564f1d24
aiden jastrzembski <aidenjastrzembski@gmail.com>
2024-11-05 15:35:39 -0500

    Add Scroll effects to LandingPage and Leaderboard page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,31 +5,43 @@ import { FeaturesCards } from "../components/FeaturesCards";
 import { EmailBanner } from "../components/EmailBanner";
 import { Faq } from "../components/faq";
 import { CarouselComponent } from "../components/Carousel";
+import { ScrollAnimation } from "../components/ScrollAnimation";
 
 const LandingPage = () => {
   return (
     <div className={"landing-page-background"}>
       <div className="card-background">
-        <Center>
-          <Card w="80%" h="15rem" shadow="lg" className="logo-card">
-            <Text ta="center" className="logo-text">
-              Scripted Reality
-            </Text>
-          </Card>
-        </Center>
+        <ScrollAnimation direction="up">
+          <Center>
+            <Card w="80%" h="15rem" shadow="lg" className="logo-card">
+              <Text ta="center" className="logo-text">
+                Scripted Reality
+              </Text>
+            </Card>
+          </Center>
+        </ScrollAnimation>
       </div>
       <div className="body">
         <Center>
-          <FeaturesCards />
-        </Center>
-        <CarouselComponent />
-        <Center>
-          <Faq />
+          <ScrollAnimation direction="up" delay={.75}>
+            <FeaturesCards />
+          </ScrollAnimation>
         </Center>
 
+        <ScrollAnimation direction="up" delay={0.3}>
+          <CarouselComponent />
+        </ScrollAnimation>
+
         <Center>
-          <EmailBanner />
+          <ScrollAnimation direction="right" delay={0.4}>
+            <Faq />
+          </ScrollAnimation>
         </Center>
+        <ScrollAnimation direction="left" delay={0.5}>
+          <Center>
+            <EmailBanner />
+          </Center>
+        </ScrollAnimation>
       </div>
     </div>
   );
