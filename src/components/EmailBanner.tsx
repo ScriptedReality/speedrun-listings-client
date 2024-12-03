34624e65ce16b38bfcdb5a99b075adf69816a373
Michael Strange <66543114+michael-strange@users.noreply.github.com>
2024-12-03 15:40:26 -0500

    Created button for adding runs on the leaderboards page (#28)
    * Created button for adding runs on the leaderboards page
    
    * Switched some components to tailwind and fixed issues with spacing in the FAQ and email newsletter sections

--- src/components/EmailBanner.tsx
+++ src/components/EmailBanner.tsx
@@ -1,30 +1,40 @@
-import { Text, Title, TextInput, Button, Image } from "@mantine/core";
+import { Text, Title, TextInput, Button, Image, Grid } from "@mantine/core";
 import image from "../assets/image.svg";
 import classes from "../styles/EmailBanner.module.css";
 
 export function EmailBanner() {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.body}>
+    <Grid grow p={5}>
+      <Grid.Col span={7}>
         <Title className={classes.title}>Wait a minute...</Title>
-        <Text fw={500} fz="lg" mb={5}>
+        <Text p={5} fw={500} fz="lg" mb={5}>
           Subscribe to our newsletter!
         </Text>
-        <Text fz="sm" c="dimmed">
+        <Text p={5} fz="sm" c="dimmed">
           You will never miss important product updates, latest news and
           community QA sessions. Our newsletter is once a week, every Sunday.
         </Text>
 
-        <div className={classes.controls}>
-          <TextInput
+        <div style={{ padding: "5px" }}>
+          <TextInput 
+            size="md"
             placeholder="Your email"
-            classNames={{ input: classes.input, root: classes.inputWrapper }}
+            style={{ marginBottom: "10px", width: "100%", maxWidth: "400px" }}
           />
-          <Button className={classes.control}>Subscribe</Button>
+          <Button>Subscribe</Button>
         </div>
-      </div>
-      <Image src={image} className={classes.image} />
-    </div>
+      </Grid.Col>
+      
+      <Grid.Col span={5}>
+        <Image 
+          p={5}
+          src={image}
+          alt="Newsletter"
+          fit="contain"
+          style={{ width: '100%', height: 'auto', maxHeight: '400px' }}
+        />
+      </Grid.Col>
+    </Grid>
   );
 }
 
